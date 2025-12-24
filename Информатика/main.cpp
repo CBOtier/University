04dc1b05f5135ef38fb73c1ac2f68d50ed6655e6
CBOtier <yegor.gryaznov@inbox.ru>
2025-12-24 23:13:58 +0300

    Add files via upload

---
+++ Информатика/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    float x, y, R, S;
+
+    cin >> x >> y;
+
+    // Вычисление R = x * log10(y)
+    R = x * log10(y);  // 
+
+    // Вычисление S = sec(x) * кубический корень из y
+    S = (1.0f / cos(x)) * cbrt(y);  
+
+    cout << R << endl;
+    cout << S << endl;
+    cout << max(R, S) << endl;
+
+    return 0;
+}
